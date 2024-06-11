f3ebe4e682cf6ae0a8566a6092c4f984fb4ef57a
bouch95 <167940846+bouch95@users.noreply.github.com>
2024-06-11 11:29:02 -0400

    Update substitution.test.js

--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,25 @@
 // Write your tests here!
+const expect = require("chai").expect;
+
+const {substitution} = require("../src/substitution");
+
+describe("substitution", () => {
+    it("Encoding, alphabet should included have exactly 26 characters, else false", () => {
+        
+        const input = "thinkful";
+        const alphabet = "xoyqmcgrukswaflnthdjpzibev";
+        const expected = "jrufscpw";
+        const actual = substitution(input, alphabet);
+        expect(actual).to.equal(expected);
+    });   
+    
+    it("Decoding, alphabet should included have exactly 26 characters, encode must include 'false', else false", () => {
+        
+        const input = "y&ii$r&";
+        const alphabet = "$wae&zrdxtfcygvuhbijnokmpl"
+        const expected = "message";
+        const actual = substitution(input, alphabet, false);
+        expect(actual).to.equal(expected);
+    });   
+    
+});
